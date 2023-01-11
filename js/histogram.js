3976393b5e85c856505c5429ccfbeca8095f1d2c
e1614852 <e1614852@student.tuwien.ac.at>
2023-01-11 22:07:48 +0100

    add transitions to histogramm

--- js/histogram.js
+++ js/histogram.js
@@ -1,13 +1,15 @@
 class Histogram{
-    constructor(width, height, volumeVoxels) {
+    bar;
+    constructor(width, height) {
         this.height = height;
         this.width = width;
-        this.volumeVoxels = volumeVoxels
     }
 
-    createHistogram() {
+    createHistogram(voxels) {
+        d3.select('svg').remove();
+        const volumeVoxels =  voxels;
         // Compute bins.
-        const bins = d3.bin().thresholds(40)(this.volumeVoxels);
+        const bins = d3.bin().thresholds(40)(volumeVoxels);
 
         this.xDomain = [bins[0].x0, bins[bins.length - 1].x1];
         this.yDomain = [0, d3.max(bins, d => d.length)];
@@ -24,20 +26,50 @@ class Histogram{
             .range([this.height, 0]);
 
         //chart size and margin
-        this.svg = d3.select("#histogram")
+        this.bar = d3.select("#histogram")
             .append("svg")
             .attr("width", this.width)
             .attr("height", this.height);
 
-        this.svg.append("g")
+        this.bar.append("g")
             .selectAll("rect")
             .data(bins)
             .join("rect")
             .attr("x", d => this.x(d.x0) + 1)
+            .attr("y", d => this.height)
+            .attr("height", d => 0)
             .attr("width", d => Math.max(0, this.x(d.x1) - this.x(d.x0) - 1))
-            .attr("y", d => this.y(d.length))
-            .attr("height", d => this.y(0) - this.y(d.length))
             .attr("fill", "white");
+
+        this.bar.selectAll("rect")
+            .transition()
+            .duration(2000)
+            .attr("y", d => this.y(d.length))
+            .attr("height", d => this.height - this.y(d.length))
+            .attr("fill", "white");
+
+
+    }
+
+     makeHist(values) {
+
+        // modify domains of axes, create histogram
+
+        // bind data
+        var rect = svg.selectAll('rect')
+            .data(histogram);
+
+        // add new elements
+        rect.enter().append('rect');
+
+        // update existing elements
+        rect.transition()
+            .duration(3000)
+            .attr('transform', '...');
+
+        // remove old elements
+        rect.exit().remove();
+
     }
 
 
--- js/vis1.js
+++ js/vis1.js
@@ -45,6 +45,7 @@ function init() {
 
     // dummy shader gets a color as input
     testShader = new TestShader([255.0, 255.0, 0.0]);
+    histogram = new Histogram(400, 200)
 }
 
 /**
@@ -92,8 +93,8 @@ async function resetVis(){
     // our camera orbits around an object centered at (0,0,0)
     orbitCamera = new OrbitCamera(camera, new THREE.Vector3(0,0,0), 2*volume.max, renderer.domElement);
 
-    histogram = new Histogram(400, 200, volume.voxels)
-    histogram.createHistogram();
+
+    histogram.createHistogram(volume.voxels);
 
     // init paint loop
     requestAnimationFrame(paint);
